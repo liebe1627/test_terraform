71f3535cd3fff6aacfc10d0a23ac761f3fe1b441
Omkar Nagnure <Omkar.Nagnure@MOTIFWORKS.IN>
2025-04-11 11:58:06 +0530

    adding the terraform file with storage account and the container

--- src/data/backend.tf
+++ src/data/backend.tf
@@ -4,7 +4,17 @@ resource "azurerm_resource_group" "rg5" {
 }
 
 resource "azurerm_storage_account" "storage1" {
-  
+  name = "${random_pet.prefix.id}-storage"
+  resource_group_name = azurerm_resource_group.rg5.name
+  location = azurerm_resource_group.rg5.location
+  account_tier = var.storage_account.tier
+  account_replication_type = var.storage_account.replication
+}
+
+resource "azurerm_storage_container" "storage_container1" {
+  name = "${azurerm_storage_account.storage1.name}-container1"
+  storage_account_id = azurerm_storage_account.storage1.id
+  container_access_type = var.container.access
 }
 
 resource "random_pet" "prefix" {
--- src/data/variables.tf
+++ src/data/variables.tf
@@ -5,3 +5,18 @@ variable "res_grp_name" {
     location = "westus"
   })
 }
+
+variable "storage_account" {
+  description = "This the information about the account tier and the replication type"
+  type = object({
+    tier = "Standard"
+    replication = "GRS"
+  })
+}
+
+variable "container" {
+  description = "This is the information about the container"
+  type = object({
+    access = "private"
+  })
+}
